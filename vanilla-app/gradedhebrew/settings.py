b0e952dce8c150cf463397c30d99b9ecd6178c23
sethbam9 <54952081+sethbam9@users.noreply.github.com>
2023-04-26 13:22:07 -0400

    Updating scripts

--- vanilla-app/gradedhebrew/settings.py
+++ vanilla-app/gradedhebrew/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-i!1fts)u1(6)aspydawx8udgg&&va6=d@77h#y1rpwoc1b6c&d"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 # DEBUG = "DJANGO_DEBUG" in os.environ and os.environ["DJANGO_DEBUG"] == "ON"
 
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sethbam9.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
