9c6f0c02398cb60a1f64c9fbed3a95803a490183
sethbam9 <54952081+sethbam9@users.noreply.github.com>
2024-05-28 23:46:11 -0400

    fix tw for collectstatic

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-	content: ["./src/app/templates/**/*.html", "./src/app/static/js/**/*.js"],
+	content: [
+		"./src/app/templates/**/*.html",
+		"./src/app/static/js/**/*.js",
+		"./src/**/*.py",
+	],
 	theme: {
 		screens: {
 			sm: "480px",
